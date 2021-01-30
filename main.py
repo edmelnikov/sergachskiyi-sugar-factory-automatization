de11a8dff8e0042d2939856bfd0c71e2647ece33
Evgeny <Kyberbote@gmail.com>
2021-01-30 22:59:15 +0300

    added kml support, wip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 import json
-
+import simplekml
 
 def get_token(login, password):
     log_resp = requests.get('http://cloud.ckat-nn.ru/ServiceJSON/Login?UserName={}&Password={}'.format(login, password))
@@ -36,7 +36,7 @@ def get_devices_id(token, schema_id):
         return -1
 
 
-def get_data_by_id(token, schema_id, device_id):
+def get_data_by_id(token, schema_id, device_id):  # на выходе - все данные
     online_info_resp = requests.get('http://cloud.ckat-nn.ru/ServiceJSON/GetOnlineInfo?session={}&schemaID={}&IDs={}'.format(
             token, schema_id, device_id))
     if online_info_resp:
@@ -47,6 +47,7 @@ def get_data_by_id(token, schema_id, device_id):
         return -1
 
 
+'''
 def get_data_all(token, schema_id, all_ids):  # !
     max_req_size = 50  # максимальное количество id, которое подается на сервер при одном запросе
     req_size = 0
@@ -89,9 +90,42 @@ def get_data_all(token, schema_id, all_ids):  # !
                 print('No info about', key)
 
     return data_all
+'''
 
 
-#------------------------------------------------------------------#
+def get_data_all(token, schema_id):  # на выходе - словарь с координатами
+    online_info_all_resp = requests.get('http://cloud.ckat-nn.ru/ServiceJSON/GetOnlineInfoAll?session={}&schemaID={}'.format(token, schema_id))
+    if online_info_all_resp:
+        online_info_all = online_info_all_resp.json()
+        coords = dict()
+        for key in online_info_all:
+            if (online_info_all[key] != None):
+                if online_info_all[key]['LastPosition']['Lat'] != 0 and online_info_all[key]['LastPosition']['Lng'] != 0:
+                    coords[key] = online_info_all[key]['LastPosition']
+                else:
+                    print('No coordinates for', key)
+            else:
+                print('No info about', key)
+        return coords
+    else:
+        print('GetOnlineInfoAll error')
+        return -1
+
+
+def add_data_to_kml(coordinates, filename):
+    kml = simplekml.Kml()
+    for key in coordinates:
+        pnt = kml.newpoint()
+        pnt.name = key
+        pnt.description = 'There is supposed to be fuel level for ' + key
+        pnt.coords = [(
+            coordinates[key]['Lng'],
+            coordinates[key]['Lat']
+        )]
+    kml.save(filename)
+
+
+# ------------------------------------------------------------------#
 login = 'Весна'
 password = '912912913'
 # token = GetToken(login, password)
@@ -103,8 +137,13 @@ if token != -1:
     print('Schema ID:', schema_id)
     print('Sсhema name:', schemas[0]['Name'])
     devices_ids = get_devices_id(token, schema_id)
-    coordinates = get_data_all(token, schema_id, devices_ids)
-    print(coordinates)
-    print(len(coordinates))
+    print(devices_ids)
+    coords = get_data_all(token, schema_id)
+    #key = '12f35d7c-34e3-45f9-a25b-338925c839ad'
+    #data = get_data_by_id(token, schema_id, key)
+    #print(data[key]['LastPosition'])
+    #coordinates = data[key]['LastPosition']
+    add_data_to_kml(coords, 'Vehicles.kml')
+
 
 
